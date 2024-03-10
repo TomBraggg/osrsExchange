d7a5b63290d740fdb6100029649078a19d33b157
Tom Bragg <tom.bragg.197@gmail.com>
2024-03-10 10:07:29 +0000

    TB - Added method to calculate item margins

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database.mapping import create_mapping_table, update_mapping_table
 from database.latest import create_latest_table, update_latest_table
 from database.five_min_history import create_five_min_history_table, update_five_min_history_table
 from database.one_h_history import create_one_hour_history_table, update_one_hour_history_table
+from item_margins.item_margins import get_item_margins
 
 
 def main():
@@ -16,6 +17,8 @@ def main():
     update_latest_table()
     update_five_min_history_table()
     update_one_hour_history_table()
+
+    get_item_margins()
 
 
 if __name__ == "__main__":
